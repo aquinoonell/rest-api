7775dbf8b53dcfed24a0b51f988921692f8a3a9b
onellaquino@icloud.com <onell@Onells-MacBook-Pro.local>
2025-02-20 10:27:52 -0500

    Created an API that can store emails from my other project

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-const app = require('express')();
-const PORT = 8080;
+const express = require ('express');
+const bodyParser = require("body-parser");
+const cors = require('cors');
+
+const app = express();
+const port = 5100;
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+let subs = [];
+
+app.post("/api/subs", (req, res) => {
+  const { email } = req.body;
+  if (!email) {
+    return res.status(400).json({ error: "Email is required. " });
+  }
+
+  if (subs.includes(email)) {
+    return res.status(400).json({ error: "Email already sub. " });
+  }
+
+  subs.push(email);
+
+  return res.status(200).json({ success: true });
+});
+
+app.get('/api/subs',(req , res) => {
+    res.json({subscriptions : subs });
+
+});
+
+app.listen(port, () => {
+    console.log('server up on localhost:${port}');
+});
