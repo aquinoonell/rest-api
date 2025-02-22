5b79968257b1f5af6c2c7b1059caf7490126c70e
onellaquino@icloud.com <onell@macbookpro.myfiosgateway.com>
2025-02-22 00:22:15 -0500

    Had to delete some branches, caused some errors, currently fixing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require ('express');
+const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 const port = 5100;
@@ -26,11 +26,10 @@ app.post("/api/subs", (req, res) => {
   return res.status(200).json({ success: true });
 });
 
-app.get('/api/subs',(req , res) => {
-    res.json({subscriptions : subs });
-
+app.get("/api/subs", (req, res) => {
+  res.json({ subscriptions: subs });
 });
 
 app.listen(port, () => {
-    console.log('server up on localhost:${port}');
+  console.log("server up on localhost:${port}");
 });
